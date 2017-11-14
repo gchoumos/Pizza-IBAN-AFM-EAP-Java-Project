0f4aa44951e732ddcb0bf341c1e84d4461e3a0a8
George Ch <g.choumos@gmail.com>
2017-11-14 02:48:40 +0200

    Functionality moved to methods - Pending file reads/writes

--- IBAN/IBAN.java
+++ IBAN/IBAN.java
@@ -14,145 +14,136 @@ import java.util.Arrays;
 
 public class IBAN {
 
-    // A Greek IBAN should be 27 characters long
 	private char[] grIBAN;
+	private char[] ibanInCheck,ibanInCheck2; // helper
+	private char[] grToNum = {'1','6','2','7'}; // helper for the GR conversion
 
-	// Mutators
-	
+	// We need an accessor for the IBAN
+	public String getIBAN() {
+		return new String(this.grIBAN);
+	}
 
-	// IBAN has already been checked if it's starting with GR. This funcrion will
-	// check whether the check digits are ok.
-	public boolean checkIBAN() {
-
-		System.out.println("Checking IBAN ...");
-
-		// The initial (27 chars long)
-		char[] ibanInCheck = new char[this.grIBAN.length];// = this.grIBAN;
-
-		/****
-			STEP 1: Move the first 4 chars to the end
-		****/
-		// move the first four characters to the end (GRDD)
+	// Move GRDD to the end
+	private void move4FirstToEnd() {
 		for(int i=0;i<this.grIBAN.length;i++) {
-			ibanInCheck[i] = this.grIBAN[(i + 4)%this.grIBAN.length];
-			//System.out.println("Iban in iteration " + i + ": " + new String(ibanInCheck));
+			this.ibanInCheck[i] = this.grIBAN[(i + 4)%this.grIBAN.length];
 		}
+	}
 
-		System.out.println("ibanInCheck after initial shift: " + new String(ibanInCheck));
+	// Convert GR to 1627
+	private void convertGRToDigits() {
+		char[] lastDigits = Arrays.copyOfRange(this.ibanInCheck,this.ibanInCheck.length-2,this.ibanInCheck.length);
 
-		/****
-			STEP 2: Convert G and R to 16 and 27 respectively
-		****/
-		char[] grToNum = {'1','6','2','7'};
-		System.out.println("Greek indication is converted to: " + new String(grToNum));
+		// We now need an array that will be 27 + 2 = 29 chars long. Use helper
+		this.ibanInCheck2 = new char[this.ibanInCheck.length + 2];
 
-		// In order to do this, we'll convert the last 4 characters GRXX to 1627XX
-		// Get the last 2 digits
-		char[] lastDigits = Arrays.copyOfRange(ibanInCheck,ibanInCheck.length-2,ibanInCheck.length);
-		System.out.println("Last Digits of IBAN after initial shift: " + new String(lastDigits));
-
-		// We now need an array that will be 27 + 2 = 29 chars long
-		char[] ibanInCheck2 = new char[this.grIBAN.length + 2];
-
-		// Now make the actual modification to the iban in check
 		for(int i=0;i<this.grIBAN.length+2;i++) {
 			if (i<this.grIBAN.length-4) {
-				ibanInCheck2[i] = ibanInCheck[i];
+				this.ibanInCheck2[i] = this.ibanInCheck[i];
 			} else if (i<this.grIBAN.length) {
-				ibanInCheck2[i] = grToNum[i-this.grIBAN.length+4];
+				this.ibanInCheck2[i] = this.grToNum[i-this.grIBAN.length+4];
 			} else {
-				ibanInCheck2[i] = lastDigits[i-this.grIBAN.length];
+				this.ibanInCheck2[i] = lastDigits[i-this.grIBAN.length];
 			}
 		}
-		System.out.println("IBAN as occured after shift and GR translation: " + new String(ibanInCheck2));
+	}
 
-		/****
-			STEP 3: Get the first 9 digits and calculate mod97
-		****/
-		String ibanPart = new String(ibanInCheck2,0,9);
-		// We calculate the mod 97 and convert it to char array
-		char[] digits = ("" + (Integer.parseInt(ibanPart) % 97)).toCharArray();
+	// This method will calculation steps of mod97 and replacements.
+	private void calculateModReplaceRepeat() {
+		// will be performed 3 times
+		for (int i=0;i<3;i++) {
+			String ibanPart;
 
-		/****
-			STEP 4: Replace those first 9 digits with the mod97
-		****/
-		char [] ibanInCheck3 = new char[ibanInCheck2.length-7];
+			// for the first 2 steps we get 9 digits, for the third one, it depends.
+			ibanPart = new String(this.ibanInCheck2,0,9);
 
-		ibanInCheck3[0] = digits[0];
-		if(digits.length==2) {
-			ibanInCheck3[1] = digits[1];
+			// We calculate the mod 97 and convert it to a char array
+			char[] digits = ("" + (Integer.parseInt(ibanPart) % 97)).toCharArray();
+
+			this.ibanInCheck = new char[this.ibanInCheck2.length-7];
+
+			this.ibanInCheck[0] = digits[0];
+			if(digits.length==2) {
+				this.ibanInCheck[1] = digits[1];
+			}
+			for(int j=digits.length;j<this.ibanInCheck.length;j++) {
+				this.ibanInCheck[j] = this.ibanInCheck2[j+7];
+			}
+
+			// Last but not least, swap the variables so that the same names
+			// will be reused in the next iteration.
+			char[] temp = new char[this.ibanInCheck2.length];
+			System.arraycopy(this.ibanInCheck2,0,temp,0,this.ibanInCheck2.length);
+			this.ibanInCheck2 = new char[this.ibanInCheck.length];
+			System.arraycopy(this.ibanInCheck,0,this.ibanInCheck2,0,this.ibanInCheck.length);
+			this.ibanInCheck = new char[temp.length];
+			System.arraycopy(temp,0,this.ibanInCheck,0,temp.length);
 		}
-		for(int i=digits.length;i<ibanInCheck3.length;i++) {
-			ibanInCheck3[i] = ibanInCheck2[i+7];
-		}
-		System.out.println("IBAN after step 4: " + new String(ibanInCheck3));
+	}
 
-		/****
-			STEP 5: Repeat step 3
-		****/
-		String ibanPart2 = new String(ibanInCheck3,0,9);
-		char[] digits2 = ("" + (Integer.parseInt(ibanPart2) % 97)).toCharArray();
-
-		/****
-			STEP 6: Repeat step 4
-		****/
-		char [] ibanInCheck4 = new char[ibanInCheck3.length-7];
-
-		ibanInCheck4[0] = digits2[0];
-		if(digits2.length==2) {
-			ibanInCheck4[1] = digits2[1];
-		}
-		for(int i=digits2.length;i<ibanInCheck4.length;i++) {
-			ibanInCheck4[i] = ibanInCheck3[i+7];
-		}
-		System.out.println("IBAN after step 6: " + new String(ibanInCheck4));
-
-		/****
-			STEP 7: Repeat step 5
-		****/
-		String ibanPart3 = new String(ibanInCheck4,0,9);
-		char[] digits3 = ("" + (Integer.parseInt(ibanPart3) % 97)).toCharArray();
-
-		/****
-			STEP 8: Repeat step 6
-		****/
-		char [] ibanInCheck5 = new char[ibanInCheck4.length-7];
-
-		ibanInCheck5[0] = digits3[0];
-		if(digits3.length==2) {
-			ibanInCheck4[1] = digits3[1];
-		}
-		ibanInCheck5[1] = digits3[1];
-		for(int i=digits3.length;i<ibanInCheck5.length;i++) {
-			ibanInCheck5[i] = ibanInCheck4[i+7];
-		}
-		System.out.println("IBAN after step 8: " + new String(ibanInCheck5));
-
-		/****
-			STEP 9: Check the final mod 97 - Should be 1 for the IBAN to be valid
-		****/
-		if(Integer.parseInt(new String (ibanInCheck5)) % 97 == 1) {
+	// Final check of given IBAN - Returns true or false
+	private boolean finalCheckIBAN() {
+		// This is actually step 9 where we calculate the final mod
+		if(Integer.parseInt(new String (this.ibanInCheck2)) % 97 == 1) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-/*
-Q W E R T
-0 3
-Q W E Q T
-1 4
-Q W E Q W
-2 0
-E W E Q W
-3 1
-E R E Q W
-4 2
-E R T Q W 
-*/
+
+	// Will return the IBAN after it got fixed
+	private void finalFixIBAN() {
+		char[] fixed = ("" + (Integer.parseInt(new String (this.ibanInCheck2))%97)).toCharArray();
+		if (Integer.parseInt(new String(fixed)) < 10) {
+			this.grIBAN[2] = 0;
+			this.grIBAN[3] = fixed[0];
+		} else {
+			this.grIBAN[2] = fixed[0];
+			this.grIBAN[3] = fixed[1];
+		}
+	}
+
+	// IBAN has already been checked if it's starting with GR. This function will
+	// check whether the check digits are ok.
+	public boolean checkIBAN() {
+
+		// The initial (27 chars long)
+		this.ibanInCheck = new char[this.grIBAN.length];// = this.grIBAN;
+
+		// STEP 1: Move the first 4 chars to the end
+		move4FirstToEnd();	
+		
+		// STEP 2: Convert G and R to 16 and 27 respectively
+		convertGRToDigits();
+		
+		// STEPS 3 to 8: Calculate mod, replace and repeat.
+		calculateModReplaceRepeat();
+		
+		// STEP 9: Return whether the given IBAN was good or not
+		return finalCheckIBAN();
+	}
+
+	// fix and invalid iban by correcting its check digits
+	public void makeIBAN() {
+
+		// The initial (27 chars long)
+		this.ibanInCheck = new char[this.grIBAN.length];// = this.grIBAN;
+
+		// STEP 1: Convert the 2 check digits to 00
+		this.grIBAN[2] = '0';
+		this.grIBAN[3] = '0';
+
+		// STEPS 2 to 8: We are re-using methods! That's a huge win!
+		move4FirstToEnd();
+		convertGRToDigits();
+		calculateModReplaceRepeat();
+
+		// STEP 9: Find the correct digits and return the fixed IBAN
+		finalFixIBAN();
+	}
+
 	IBAN(String incomingIBAN) {
 		this.grIBAN = incomingIBAN.toCharArray();
-		System.out.println("Initial IBAN: " + new String(this.grIBAN));
 	}
 	
 }
--- IBAN/IBANApp.java
+++ IBAN/IBANApp.java
@@ -1,6 +1,8 @@
 /**
   TODOs:
-    1. 
+    1. We could catch and print both format errors as well using a flag. For example
+	   an IBAN candidate could be 26 characters long AND not starting with GR. In
+	   that case we could print both errors but maybe that's an overkill.
 **/
 
 import java.io.*;
@@ -8,14 +10,43 @@ import java.util.Scanner;
 
 public class IBANApp {
 
-	public static void main(String[] args) {
-		// This should be converted to be reading from a file
-		IBAN ib = new IBAN("GR3902604290000780200174634");
-
-		if (ib.checkIBAN()) {
-			System.out.println("IBAN is good!");
+	private boolean checkFormat(String ibanToCheck) {
+		if (ibanToCheck.length() != 27) {
+			System.out.println("IBAN given not 27 characters long!");
+			return false;
+		} else if (ibanToCheck.charAt(0) != 'G' || ibanToCheck.charAt(1) != 'R') {
+			System.out.println("IBAN given does not begin with 'GR'!");
+			return false;
 		} else {
-			System.out.println("IBAN is not valid. We are fixing it ...");
+			return true;
+		}
+	}
+
+	public static void main(String[] args) {
+
+		// To be able to use the non-static method checkFormat
+		IBANApp app = new IBANApp();
+		
+		// This should be converted to be reading from a file
+		String ibanString = new String("GR3902604290000780200174634");
+
+		// We have to check first if the format is good. If not we skip the
+		// rest of the checks completely and move on to the next one
+		if (app.checkFormat(ibanString)) {
+			
+			IBAN ib = new IBAN(ibanString);
+
+			System.out.println("Checking IBAN: " + ib.getIBAN());
+			if (ib.checkIBAN()) {
+				System.out.println("IBAN is good!");
+			} else {
+				System.out.println("IBAN is not valid. We are fixing it ...");
+				ib.makeIBAN();
+				System.out.println("IBAN fixed --> " + ib.getIBAN());
+			}
+		} else {
+			System.out.println("Format is not good. Moving on to the next one ...");
+			// once the for loop gets in, continue here (not really necessary)
 		}
 	}
 }
